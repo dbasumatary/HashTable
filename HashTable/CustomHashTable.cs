fe76e60d924af4b675e4a7ea7a8de0b2f14f69bd
dbasumatary <basudhondip@gmail.com>
2023-06-03 17:17:34 +0530

    Added: Add method in HashTable operation

---
+++ HashTable/CustomHashTable.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable
+{
+    public class CustomHashTable<T>
+    {
+        private class Node
+        {
+            public T Data;
+            public Node Next;
+
+            public Node(T data)
+            {
+                Data = data;
+            }
+        }
+
+        private Node[] Names;
+        
+        public CustomHashTable(int size)
+        {
+            Names = new Node[size];
+        }
+
+        //Add method 
+        public void Add(T name)
+        {
+            Node newNode = new Node(name);
+            int arrayIndex = Math.Abs(name.GetHashCode()) % Names.Length; //Number between 0 to size of array
+            Names[arrayIndex] = newNode;
+        }
+    }
+}
--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hashtable operation");
+            CustomHashTable<int> customHash = new CustomHashTable<int>(10);
+            customHash.Add(31);
+            customHash.Add(12);
+            customHash.Add(29);
+            customHash.Add(41);
+            customHash.Add(23);
+
         }
     }
 }
